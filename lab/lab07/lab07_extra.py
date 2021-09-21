23ef699cf688cc95acbf8656a0dc53a3750a834c
capxing <1501901576@qq.com>
2021-09-21 18:40:14 +0800

    lab07 commit

---
+++ lab/lab07/lab07_extra.py
@@ -0,0 +1,116 @@
+""" Optional Questions for Lab 07 """
+
+from lab07 import *
+
+# Q6
+def remove_all(link , value):
+    """Remove all the nodes containing value. Assume there exists some
+    nodes to be removed and the first element is never removed.
+
+    >>> l1 = Link(0, Link(2, Link(2, Link(3, Link(1, Link(2, Link(3)))))))
+    >>> print(l1)
+    <0 2 2 3 1 2 3>
+    >>> remove_all(l1, 2)
+    >>> print(l1)
+    <0 3 1 3>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    """
+    "*** YOUR CODE HERE ***"
+    while link.rest:
+        while link.second == value:
+            link.rest = link.rest.rest
+            if link.rest == Link.empty:
+                return
+        link = link.rest
+
+# Q7
+def deep_map_mut(fn, link):
+    """Mutates a deep link by replacing each item found with the
+    result of calling fn on the item.  Does NOT create new Links (so
+    no use of Link's constructor)
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> deep_map_mut(lambda x: x * x, link1)
+    >>> print(link1)
+    <9 <16> 25 36>
+    """
+    "*** YOUR CODE HERE ***"
+    if link:
+        if isinstance(link, int):
+            link.first = fn(link.first)
+        else:
+            deep_map_mut(fn, link.first)
+        link = link.rest
+
+
+# Q8
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    log = set()
+    while link:
+        if link in log:
+            return True
+        log.add(link)
+        link = link.rest
+    return False
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    turtle = link
+    rabbit = link.rest
+    while rabbit:
+        if turtle is rabbit:
+            return True
+        turtle, rabbit = turtle.rest, rabbit.rest.rest
+    return False
+# Q9
+def reverse_other(t, depth = 0):
+    """Mutates the tree such that nodes on every other (even_indexed) level
+    have the labels of their branches all reversed.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])]), Tree(8)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
+    """
+    "*** YOUR CODE HERE ***"
+    if depth % 2 == 1:
+        for b in t.branches:
+            reverse_other(b, depth + 1)
+    else:
+        labels = [b.label for b in t.branches]
+        for index, b in enumerate(t.branches):
+            b.label = labels[-1-index]
+            reverse_other(b, depth + 1)
+
